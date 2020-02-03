b3d9aa88991a42adc715ffd2884d4f6bbc87e534
Kamil Pinas <kamilpinas@gmail.com>
2020-02-03 11:48:12 +0100

    // new request put

--- src/com/pdfgenerator/model/AnswerData.java
+++ src/com/pdfgenerator/model/AnswerData.java
@@ -8,13 +8,13 @@ public class AnswerData implements Serializable {
 
     private int questionId;
     private boolean lastQuestion;
-    private Integer[] selectedAnswers;
+    private String selectedAnswers;
 
     public AnswerData() {
     }
 
 
-    public AnswerData(int questionId, boolean lastQuestion, Integer[] selectedAnswers) {
+    public AnswerData(int questionId, boolean lastQuestion, String selectedAnswers) {
         this.questionId=questionId;
         this.lastQuestion = lastQuestion;
         this.selectedAnswers = selectedAnswers;
@@ -28,11 +28,11 @@ public class AnswerData implements Serializable {
         this.lastQuestion = lastQuestion;
     }
 
-    public Integer[] getSelectedAnswers() {
+    public String getSelectedAnswers() {
         return selectedAnswers;
     }
 
-    public void setSelectedAnswers(Integer[] answers) {
+    public void setSelectedAnswers(String answers) {
         this.selectedAnswers = selectedAnswers;
     }
 
@@ -47,9 +47,9 @@ public class AnswerData implements Serializable {
     @Override
     public String toString() {
         return "{\"AnswerData\":{"
-                + ", \"questionId\":\"" + questionId + "\""
-              + ", \"selectedAnswers\":" + Arrays.toString(selectedAnswers) //TODO:?
+                + "\"questionId\":\"" + questionId + "\""
                 + ", \"lastQuestion\":\"" + lastQuestion + "\""
+                + ", \"selectedAnswers\":\"" + selectedAnswers + "\""
                 + "}}";
     }
 }
--- src/com/pdfgenerator/model/NetworkRequests.java
+++ src/com/pdfgenerator/model/NetworkRequests.java
@@ -4,12 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.util.Arrays;
 
@@ -41,4 +49,44 @@ public class NetworkRequests {
             throw new Exception("Problem z zwróceniem JSONA");
         }
     }
+
+    public static void answerData(final AnswerData answers) throws Exception{
+
+        final CloseableHttpClient client = HttpClients.createDefault();
+        final HttpPut httpPut = new HttpPut("http://127.0.0.1:8080/quiz/calculate");
+        Gson gson = new Gson();
+        // Serializacja obiektu do JSONa
+        final String json = gson.toJson(answers);
+        try {
+            final StringEntity entity = new StringEntity(json);
+            httpPut.setEntity(entity);
+            httpPut.setHeader("Accept", "application/json");
+            httpPut.setHeader("Content-type", "application/json");
+            final CloseableHttpResponse response = client.execute(httpPut);
+            System.out.println("answerData - kod odpowiedzi serwera: " + response.getStatusLine().getStatusCode());
+
+            if (response.getStatusLine().getStatusCode() == 400)
+            {
+                System.err.println("#BLAD 400");
+            }
+            else if (response.getStatusLine().getStatusCode() == 200)
+            {
+                System.err.println("Gitara jest #200");
+            }
+
+            client.close();
+        } catch (UnsupportedEncodingException e) {
+
+            System.out.println("Houston, we have a problem with POST unsupported encoding");
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+
+            System.out.println("Houston, we have a problem with POST client protocol");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Houston, we have a problem with POST input output");
+            e.printStackTrace();
+        }
+    }
 }
+

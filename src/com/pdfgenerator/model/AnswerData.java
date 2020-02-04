3d1e83e0d0c3b2428760b522fd682c9adda2b370
Gabriela Jasnosz <gabriela.jasnosz@gmail.com>
2020-02-04 17:21:15 +0100

    ostatni commit, koniec

--- src/com/pdfgenerator/model/AnswerData.java
+++ src/com/pdfgenerator/model/AnswerData.java
@@ -5,45 +5,31 @@ import java.util.Arrays;
 
 
 public class AnswerData implements Serializable {
-
     private int questionId;
     private boolean lastQuestion;
     private Integer[] selectedAnswers;
-
-    public AnswerData() {
-    }
-
-
+    public AnswerData() { }
     public AnswerData(int questionId, boolean lastQuestion, Integer[] selectedAnswers) {
         this.questionId = questionId;
         this.lastQuestion = lastQuestion;
         this.selectedAnswers = selectedAnswers;
     }
-
     public int getQuestionId() {
         return questionId;
     }
-
-    public void setQuestionId(int questionId) {
-        this.questionId = questionId;
-    }
-
+    public void setQuestionId(int questionId) { this.questionId = questionId; }
     public boolean isLastQuestion() {
         return lastQuestion;
     }
-
     public void setLastQuestion(boolean lastQuestion) {
         this.lastQuestion = lastQuestion;
     }
-
     public Integer[] getSelectedAnswers() {
         return selectedAnswers;
     }
-
     public void setSelectedAnswers(Integer[] selectedAnswers) {
         this.selectedAnswers = selectedAnswers;
     }
-
     @Override
     public String toString() {
         return "{\"AnswerData\":{"
--- src/com/pdfgenerator/model/QuestionData.java
+++ src/com/pdfgenerator/model/QuestionData.java
@@ -11,50 +11,25 @@ public class QuestionData implements Serializable {
     private String question;
     private boolean lastQuestion;
     private String[] answers;
-
-    public QuestionData() {
-    }
-
-
+    public QuestionData() { }
     public QuestionData(String points, String question, boolean lastQuestion, String[] answers) {
         this.points = points;
         this.question = question;
         this.lastQuestion = lastQuestion;
         this.answers = answers;
     }
-
     public String getPoints() {
         return points;
     }
-
-    public void setPoints(String points) {
-        this.points = points;
-    }
-
     public String getQuestion() {
         return question;
     }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
     public boolean isLastQuestion() {
         return lastQuestion;
     }
-
-    public void setLastQuestion(boolean lastQuestion) {
-        this.lastQuestion = lastQuestion;
-    }
-
     public String[] getAnswers() {
         return answers;
     }
-
-    public void setAnswers(String[] answers) {
-        this.answers = answers;
-    }
-
     @Override
     public String toString() {
         return "{\"QuestionData\":{"
